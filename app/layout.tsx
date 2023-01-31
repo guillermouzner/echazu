e3add08ec33a8a0d5b434a8e8aaeb224e312890d
guillermouzner <guillermouz16@gmail.com>
2023-01-31 11:38:26 -0300

    initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,14 @@
+import "./globals.css";
+
+import {Inter} from "@next/font/google";
+
+const inter = Inter({subsets: ["latin"]});
+
+export default function RootLayout({children}: {children: React.ReactNode}) {
+  return (
+    <html className={inter.className}>
+      <head />
+      <body>{children}</body>
+    </html>
+  );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,11 @@
 module.exports = {
   darkMode: "class",
   content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    // "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
     // Or if using `src` directory:
-    "./src/**/*.{js,ts,jsx,tsx}",
+    // "./src/**/*.{js,ts,jsx,tsx}",
   ],
   plugins: [],
 };
